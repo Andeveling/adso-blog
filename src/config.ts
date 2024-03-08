8040987ab7d86b95b97df6f316109d3260408619
Fabian Andres Parra Sanchez <andeveling@gmail.com>
2024-03-08 11:38:49 -0500

    primer commit

---
+++ src/config.ts
@@ -0,0 +1,70 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://adso-blog.pages.dev/", // replace this with your deployed domain
+  author: "Fabian Andres Parra Sanchez",
+  desc: "Blog para el registro del aprendizaje de la carrera de Análisis y Desarrollo de Software.",
+  title: "ADSO",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+  scheduledPostMargin: 60 * 60 * 1000, // 60 minutes
+};
+
+export const LOCALE = {
+  lang: "es", // html lang code. Set this empty and default will be "en"
+  langTag: ["es-CO"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/Andeveling/adso-blog",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/andrespsanchez/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:andeveling@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/Andeveling",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: true,
+  },
+  {
+    name: "YouTube",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "3007743602",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+
+  {
+    name: "Discord",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+];
